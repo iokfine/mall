8c233b9b12f8d9f05b555a0faef03fd597411015
HuoStar <823977362@qq.com>
2018-08-31 13:17:46 +0800

    update

--- src/store.js
+++ src/store.js
@@ -9,11 +9,6 @@ const state = {
   count: 0
 }
 
-// mutations are operations that actually mutates the state.
-// each mutation handler gets the entire state tree as the
-// first argument, followed by additional payload arguments.
-// mutations must be synchronous and can be recorded by plugins
-// for debugging purposes.
 const mutations = {
   increment (state) {
     state.count++
