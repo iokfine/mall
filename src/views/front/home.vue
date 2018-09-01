197a7d5e6e83e507656aefd5a9b3c439d053480e
nicolas <nicolas211@foxmail.com>
2018-09-02 02:03:14 +0800

    update home

---
+++ src/views/front/home.vue
@@ -0,0 +1,129 @@
+<style scoped lang="less">
+    .demo-carousel {
+        height: 350px;
+    }
+
+    .container {
+        padding: 10px 50px 0 70px;
+        .content {
+            .recommend {
+                margin: 30px 0;
+            }
+        }
+        .assist-bar {
+            margin-left: 30px;
+            .card {
+            }
+        }
+    }
+
+</style>
+
+<template>
+    <Row class="container">
+        <Col span="17" class="content">
+            <Carousel autoplay v-model="value2" loop>
+                <CarouselItem>
+                    <div class="demo-carousel" style="background-color: #19be6b;"></div>
+                </CarouselItem>
+                <CarouselItem>
+                    <div class="demo-carousel" style="background-color: #1c2438;"></div>
+                </CarouselItem>
+                <CarouselItem>
+                    <div class="demo-carousel" style="background-color: #ff9900;"></div>
+                </CarouselItem>
+                <CarouselItem>
+                    <div class="demo-carousel" style="background-color: #ed3f14;"></div>
+                </CarouselItem>
+            </Carousel>
+            <h1 style="margin-top: 20px">&nbsp;&nbsp;&nbsp;&nbsp;# Recommend article</h1>
+            <ul class="recommend" style="list-style: none">
+                <li>
+                    <Card dis-hover style="margin: 10px 10px">
+                        <h2>[情感]我喜欢你</h2>
+                        <strong>我其实真的超喜欢你的</strong>
+                        <div style="margin-top: 20px">
+                            2018年9月2号
+                        </div>
+                    </Card>
+                </li>
+                <li>
+                    <Card dis-hover style="margin: 10px 10px">
+                        <h2>[情感]我喜欢你</h2>
+                        <strong>我其实真的超喜欢你的</strong>
+                        <div style="margin-top: 20px">
+                            2018年9月2号
+                        </div>
+                    </Card>
+                </li>
+                <li>
+                    <Card dis-hover style="margin: 10px 10px">
+                        <h2>[情感]我喜欢你</h2>
+                        <strong>我其实真的超喜欢你的</strong>
+                        <div style="margin-top: 20px">
+                            2018年9月2号
+                        </div>
+                    </Card>
+                </li>
+                <li>
+                    <Card dis-hover style="margin: 10px 10px">
+                        <h2>[情感]我喜欢你</h2>
+                        <strong>我其实真的超喜欢你的</strong>
+                        <div style="margin-top: 20px">
+                            2018年9月2号
+                        </div>
+                    </Card>
+                </li>
+                <li>
+                    <Card dis-hover style="margin: 10px 10px">
+                        <h2>[情感]我喜欢你</h2>
+                        <strong>我其实真的超喜欢你的</strong>
+                        <div style="margin-top: 20px">
+                            2018年9月2号
+                        </div>
+                    </Card>
+                </li>
+                <li>
+                    <Card dis-hover style="margin: 10px 10px">
+                        <h2>[情感]我喜欢你</h2>
+                        <strong>我其实真的超喜欢你的</strong>
+                        <div style="margin-top: 20px">
+                            2018年9月2号
+                        </div>
+                    </Card>
+                </li>
+            </ul>
+        </Col>
+        <Col span="6" class="assist-bar">
+            <Card :bordered="false" class="card">
+                <p slot="title">No border title</p>
+                <p>Content of no border type. Content of no border type. Content of no border type. Content of no border
+                    type. </p>
+            </Card>
+            <Card :bordered="false" style="margin-top: 50px">
+                <p slot="title">No border title</p>
+                <p>Content of no border type. Content of no border type. Content of no border type. Content of no border
+                    type. </p>
+            </Card>
+        </Col>
+        <BackTop></BackTop>
+    </Row>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                value2: 0,
+            }
+        },
+        methods: {},
+
+        mounted() {
+            let data = {
+                currentPage: "listing"
+            }
+            this.$emit("whichPage-ctpEvent", data)
+        }
+
+    }
+</script>
--- src/views/main.vue
+++ src/views/main.vue
@@ -1,76 +1,80 @@
 <style scoped>
-    .layout-footer-center{
+    .layout-footer-center {
         margin-bottom: 0;
         text-align: center;
         background-color: #fff;
         border-top: #dddee1 1px solid;
     }
-    .header-color{
+
+    .header-color {
         background-color: #fff;
         border-bottom: #dddee1 1px solid;
     }
-    .content-pozition-center{
+
+    .content-pozition-center {
         text-align: center;
     }
-    .header-title-font{
-        font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+
+    .header-title-font {
         color: #657180;
         font-size: 14px;
     }
 
-    .content-height{
-        height: 830px;
-    }
 </style>
 <template>
     <Layout>
         <Header class="header-color">
-            <Row type="flex" align="middle" justify="center" class="content-pozition-center header-title-font">
-                <Col span="4">
-                    <router-link to="/login" style="">干货</router-link>
-                </Col>
-                <Col span="4">
-                    <router-link to="/login" style="">问答</router-link>
-                </Col>
-                <Col span="8" style="align-items: center;display: flex;justify-content: center;">
+            <Row type="flex" class="content-pozition-center header-title-font">
+                <Col span="4" style="align-items: center;display: flex;justify-content: center;">
                     <img src="../images/logo_main.png" style="height: 36px;"/>
                 </Col>
-                <Col span="4">
-                    <router-link to="/login" style="">登录</router-link>
+                <Col span="2">
+                    <router-link to="/" style="">Home</router-link>
                 </Col>
-                <Col span="4">
-                    <router-link to="/login" style="">注册</router-link>
+                <Col span="2">
+                    <router-link to="/login" style="">Essay</router-link>
+                </Col>
+                <Col span="2">
+                    <router-link to="/login" style="">About Me</router-link>
+                </Col>
+                <Col span="2">
+                    <router-link to="/login" style="">Advice</router-link>
+                </Col>
+                <Col span="7">
+                </Col>
+                <Col span="5" style="display: flex;align-items: center;">
+                    <Input search enter-button placeholder="search content..." />
                 </Col>
             </Row>
         </Header>
-        <Content class="content-height">
+        <Content>
             <router-view @whichPage-ctpEvent="ctpEvent"></router-view>
         </Content>
-        <Footer class="layout-footer-center" >2017-2018 &copy; iokfine 世界改变代码!</Footer>
+        <Footer class="layout-footer-center">2017-2018 &copy; iokfine 世界改变代码!</Footer>
     </Layout>
 </template>
 <script>
     export default {
-        data(){
-            return{
+        data() {
+            return {
                 value2: 0
             }
         },
-        methods:{
-            ctpEvent(data){
-                switch ( data.currentPage){
-                    case 'listing':{
+        methods: {
+            ctpEvent(data) {
+                switch (data.currentPage) {
+                    case 'listing': {
 
                         break
                     }
-                    case 'material':{
+                    case 'material': {
 
                         break
                     }
                 }
             }
         },
-        mounted(){
+        mounted() {
 
         }
     }
